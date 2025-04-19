64cdef89a52c13eca901869f65a9705b9bad22ad
Skips173 <Skips_173@163.com>
2025-04-19 22:46:41 +0800

    add sth

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,41 @@
-function Test() { /* Funky */
-    fetch("./texts/Template.json")
+function createDiv(inputObject) {
+    "use strict";
+
+    const uTime = inputObject.time;
+    const uFrom = inputObject.from;
+    const uText = inputObject.text;
+
+    const timeP = document.createElement("p");
+    timeP.innerHTML = "Time: " + uTime;
+    const fromP = document.createElement("p");
+    fromP.innerHTML = "From: " + uFrom;
+    const textP = document.createElement("p");
+    textP.innerHTML = "Text: " + uText;
+
+    const newDiv = document.createElement("div");
+    newDiv.classFrom = "Text";
+    newDiv.appendChild(timeP);
+    newDiv.appendChild(fromP);
+    newDiv.appendChild(textP);
+
+    const parentDiv = document.getElementById("Texts");
+    parentDiv.appendChild(newDiv);
+    return newDiv;
+}
+
+function insertTexts(arr) {
+    "use strict";
+    console.log(arr);
+    for (let i = 0; i < arr.length; ++i) {
+        createDiv(arr[i]);
+    }
+    return true;
+}
+
+function readFile() { // 获取文本所对应的JavaScript对象
+    "use strict";
+    fetch("./texts/data.json")
         .then(Response => Response.json())
-        .then(Data => {
-            document.getElementById("text1").innerHTML =
-                "Time:" + Data.Time
-                + "<br />From:" + Data.From
-                + "<br />Text:" + Data.Text;
-        });
+        .then(Data => insertTexts(Data));
+    return true;
 }
